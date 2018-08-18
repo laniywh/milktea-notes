62ee435e033c34814bc39b280bbe41b63706c90b
laniywh <laniywh@gmail.com>
2018-08-18 10:56:04 -0700

    working api request and hide api key

--- actions/store_actions.js
+++ actions/store_actions.js
@@ -1,21 +1,27 @@
 import axios from "axios";
 import qs from "qs";
+import { API_KEY } from 'react-native-dotenv';
 
 import { FETCH_STORES } from "./types";
 
-const STORE_ROOT_URL = "https://api.yelp.com/v3/businesses/search";
+const STORE_ROOT_URL = "https://api.yelp.com/v3/businesses/search?";
 
 const buildStoresUrl = region => {
   const latitude = region.latitude;
   const longitude = region.longitude;
-  const query = qs.stringify({ latitude, longitude, region });
+  const query = qs.stringify({ latitude, longitude });
   return `${STORE_ROOT_URL}${query}`;
 };
 
 export const fetchStores = region => async dispatch => {
   try {
     const url = buildStoresUrl(region);
-    let { data } = await axios.get(url);
+    console.log(API_KEY);
+    let { data } = await axios.get(
+      url,
+      {headers: {
+        "Authorization": `Bearer ${API_KEY}`
+      }});
     console.log(data);
     dispatch({ type: FETCH_STORES, payload: data });
   } catch (e) {
