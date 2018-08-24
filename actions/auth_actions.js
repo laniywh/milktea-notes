d25cb4c8d1aff9bcfb137ee6d5fbca98d2cc2d97
Lani Huang <laniywh@gmail.com>
2018-08-23 22:38:26 -0700

    facebook auth

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,7 +1,12 @@
 import { AsyncStorage } from "react-native"; // similar to local storage
-import { Google } from "expo";
+import { Google, Facebook } from "expo";
 
-import { GOOGLE_LOGIN_SUCCESS, GOOGLE_LOGIN_FAIL } from "./types";
+import {
+  GOOGLE_LOGIN_SUCCESS,
+  GOOGLE_LOGIN_FAIL,
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
+} from "./types";
 import {
   GOOGLE_IOS_CLIENT_ID,
   GOOGLE_ANDROID_CLIENT_ID
@@ -11,23 +16,40 @@ import {
 // AsyncStorage.setItems('fb_token', token);
 // AsyncStorage.getItem('fb_token');
 
-export const googleLogin = () => async dispatch => {
-  console.log("in googleLogin...");
+// export const googleLogin = () => async dispatch => {
+//   console.log("in googleLogin...");
 
-  let token = await AsyncStorage.getItem("g_token");
-  console.log("done checking token");
+//   let token = await AsyncStorage.getItem("g_token");
+//   console.log("done checking token");
 
-  if (token) {
-    // dispatch an action saying google login is done
-    console.log(token);
-    dispatch({ type: GOOGLE_LOGIN_SUCCESS, payload: token });
-  } else {
-    // start up google login process
-    doGoogleLogin(dispatch);
-  }
+//   if (token) {
+//     // dispatch an action saying google login is done
+//     console.log(token);
+//     dispatch({ type: GOOGLE_LOGIN_SUCCESS, payload: token });
+//   } else {
+//     // start up google login process
+//     doGoogleLogin(dispatch);
+//   }
+// };
+
+// for some reason, google login doesn't work.
+// after login in, the app reloads and not logged in.
+export const googleLogin = () => dispatch => {
+  console.log("in googlelogin...");
+  AsyncStorage.getItem("g_token").then(token => {
+    console.log("in callback");
+    if (token) {
+      // dispatch an action saying google login is done
+      console.log(token);
+      dispatch({ type: GOOGLE_LOGIN_SUCCESS, payload: token });
+    } else {
+      // start up google login process
+      doGoogleLogin(dispatch);
+    }
+  });
 };
 
-const doGoogleLoin = async dispatch => {
+const doGoogleLogin = async dispatch => {
   console.log("logging in...");
   try {
     const { type, accessToken } = await Google.logInAsync({
@@ -37,13 +59,46 @@ const doGoogleLoin = async dispatch => {
     });
 
     if (type === "success") {
-      console.log(result);
+      console.log(accessToken);
       await AsyncStorage.setItem("g_token", accessToken);
       dispatch({ type: GOOGLE_LOGIN_SUCCESS, payload: accessToken });
     } else {
+      console.log("fail");
       dispatch({ type: GOOGLE_LOGIN_FAIL });
     }
   } catch (e) {
-    return { error: true };
+    console.log(e);
+    dispatch({ type: GOOGLE_LOGIN_FAIL });
   }
+
+  console.log("done");
+};
+
+export const facebookLogin = () => async dispatch => {
+  console.log("check if logged in...");
+  let token = await AsyncStorage.getItem("fb_token");
+
+  if (token) {
+    console.log("logged in!");
+    // Dispatch an action saying FB login is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  } else {
+    // Start up FB Login process
+    doFacebookLogin(dispatch);
+  }
+};
+
+const doFacebookLogin = async dispatch => {
+  console.log("doing facebook login...");
+  const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+    "255752211813653",
+    { permissions: ["public_profile"] }
+  );
+
+  if (type === "cancel") {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsyncStorage.setItem("fb_token", token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 };
--- reducers/auth_reducer.js
+++ reducers/auth_reducer.js
@@ -1,12 +1,21 @@
 const INITIAL_STATE = { token: "" };
-import { GOOGLE_LOGIN_SUCCESS, GOOGLE_LOGIN_FAIL } from "./types";
+import {
+  GOOGLE_LOGIN_SUCCESS,
+  GOOGLE_LOGIN_FAIL,
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
+} from "../actions/types";
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case GOOGLE_LOGIN_SUCCESS:
-      return { ...state, token: action.payload };
+      return { token: action.payload };
     case GOOGLE_LOGIN_FAIL:
-      return state;
+      return { token: null };
+    case FACEBOOK_LOGIN_SUCCESS:
+      return { token: action.payload };
+    case FACEBOOK_LOGIN_FAIL:
+      return { token: null };
     default:
       return state;
   }
