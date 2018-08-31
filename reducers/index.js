80388eb64248704738ddad9a28a5d04fa19ee231
Lani Huang <laniywh@gmail.com>
2018-08-30 23:10:09 -0700

    show add note button on store screen when no notes

--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import stores from "./stores_reducer";
 import auth from "./auth_reducer";
+import notes from "./notes_reducer";
 
 export default combineReducers({
   stores,
-  auth
+  auth,
+  notes
 });
--- screens/StoreScreen.js
+++ screens/StoreScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import { Header } from "react-native-elements";
+import { connect } from "react-redux";
+import { Icon } from "react-native-elements";
 import StoreInfo from "../components/StoreInfo";
 
 class StoreScreen extends Component {
   render() {
+    console.log(this.props);
     const store = this.props.navigation.getParam("store", {});
 
     return (
@@ -13,6 +15,7 @@ class StoreScreen extends Component {
         <View style={styles.header}>
           <StoreInfo {...store} />
         </View>
+        {showNotes(this.props.notes, store.id)}
       </View>
     );
   }
@@ -25,7 +28,40 @@ const styles = {
     paddingLeft: 10,
     paddingRight: 10,
     paddingBottom: 10
+  },
+  notesContainer: {
+    justifyContent: "center",
+    alignItems: "center"
   }
 };
 
-export default StoreScreen;
+function showNotes(notes, storeId) {
+  if (!notes.storeId) {
+    // no notes under current store
+    return (
+      <View style={styles.notesContainer}>
+        <Icon name="circle-with-plus" type="entypo" size={70} color="#BDBDBD" />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Notes</Text>
+      <Text>Notes</Text>
+      <Text>Notes</Text>
+      <Text>Notes</Text>
+    </View>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    notes: state.notes
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(StoreScreen);
